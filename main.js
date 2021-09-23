358610efd6da4d8c8c3d080170eb1982dec4b9e5
Will Peddie <peddiew@gmail.com>
2021-09-22 22:17:03 -0600

    jsbutton

--- main.js
+++ main.js
@@ -13,3 +13,21 @@ menuCloseButton.addEventListener("click", closeMenu);
 function closeMenu() {
   menuElement.classList.remove('menu-open');
 }
+
+
+
+
+// there is an element on my page with the ID "click-to-start"
+// declare a variable named 'button'
+// assign that element to that variable
+// using document.querySelector() - JS function that selects an element with the matching CSS selector
+const button = document.querySelector("#click-to-start");
+
+// when user clicks on button (which is element with ID 'click-to-start')
+button.onclick = function() {
+  // declare variable called `name` which will store user input in window prompt
+  let name = prompt("What's your name?");
+
+  // window alert with text "Hello, [name]!" - string interpolation
+  alert(`Hello, ${name}!`);
+}
